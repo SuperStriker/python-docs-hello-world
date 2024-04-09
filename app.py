e6453485846b966d5006f3553ef4ea3fbb49972e
Anoop Martin <33829132+SuperStriker@users.noreply.github.com>
2024-04-08 22:23:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/get_data")
+def getdata():
+    data = {
+        'name' : 'My Name',
+        'url' : 'My URL'
+    }
+    return json.dumps(data)
+
+if __name__ == "__main__":
+    app.run()
